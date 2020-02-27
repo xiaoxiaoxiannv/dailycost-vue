b5c8cfff56b80500515acaf0b09b735927261c69
Luna <1017929760@qq.com>
2020-02-27 18:31:13 +0800

    重复页面布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
-        Labels.vue
-        <Nav />
+    <div class="nav-wrapper">
+        <div class="content">
+            Labels.vue
+        </div>
+        <Nav/>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Labels'
-    };
+  export default {
+    name: 'Labels'
+  };
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        border: 1px solid blue;
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
