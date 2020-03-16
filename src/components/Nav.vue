5ea9f9ef3fcbe5935722116a7ebf2476f18aa9ff
Luna <1017929760@qq.com>
2020-03-16 22:13:35 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
     <nav>
-        <router-link to="/money" class="item" active-class="selected">
-            <Icon name="money"/>
-            记账
-        </router-link>
         <router-link to="/labels" class="item" active-class="selected">
             <Icon name="label"/>
             标签
+        </router-link>
+        <router-link to="/money" class="item" active-class="selected">
+            <Icon name="money"/>
+            记账
         </router-link>
         <router-link to="/statistics" class="item" active-class="selected">
             <Icon name="statistics"/>
@@ -22,6 +22,7 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/assets/style/helper.scss";
     nav{
         display: flex;
         box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -41,7 +42,7 @@
             }
         }
         > .item.selected{
-            color: red;
+            color: $color-highlight;
         }
     }
 </style>
