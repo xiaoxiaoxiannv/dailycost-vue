ae5f9ed29cd277b9f9a60acefa56ee881f584492
Luna <1017929760@qq.com>
2020-03-30 20:28:55 +0800

    按照 vue-class-component的文档，使用props失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,51 @@
 <template>
     <div>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type === '-' && 'selected'"
+                @click="selectType('-')">支出
+            </li>
+            <li :class="type === '+' && 'selected'"
+                @click="selectType('+')">收入
+            </li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Types'
-  };
+    import Vue from 'vue'
+    import {Component} from 'vue-property-decorator';
+
+    @Component({
+      props: {
+        propMessage: String
+      }
+      })
+    export default class Types extends Vue {
+        type = '-';//'-'表示支出,'+'表示收入
+        helloMsg = 'Hello,' + this.propMessage;
+        selectType(type: string) {//type只能是'-'和'+'中的一个
+            if (type !== '-' && type !== '+') {
+                throw new Error('type is unknown')
+            }
+            this.type = type;
+        }
+    }
+  // export default {
+  //   name: 'Types',
+  //   data(){
+  //     return {
+  //       type: '-'//'-'表示支出,'+'表示收入
+  //     }
+  //   },
+  //   methods: {
+  //     selectType(type) {//type只能是'-'和'+'中的一个
+  //       if(type !== '-' && type !== '+'){
+  //         throw new Error('type is unknown')
+  //       }
+  //       this.type = type
+  //     }
+  //   }
+  // };
 </script>
 
 <style lang="scss" scoped>
