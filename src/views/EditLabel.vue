7a109f1f7183175dda024f083252fd206f103259
Luna <1017929760@qq.com>
2020-04-10 16:03:31 +0800

    展示tag的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
             <span class="rightIcon"></span>
         </div>
         <div class="form-wrapper">
-            <FormItem field-name="标签名" placeholder="请输入标签名"/>
+            <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
         </div>
         <div class="button-wrapper">
             <Button>删除标签</Button>
@@ -20,19 +20,22 @@
   import tagListModel from '@/models/tagListModel';
   import FormItem from '@/components/Money/FormItem.vue';
   import Button from '@/components/Button.vue';
+
   @Component({
     components: {Button, FormItem}
   })
   export default class EditLabel extends Vue {
-    created(){
+    tag?: { id: string; name: string } = undefined;
+
+    created() {
       const id = this.$route.params.id;
       tagListModel.fetch();
       const tags = tagListModel.data;
-      const tag = tags.filter(t => t.id ===id)[0];
-      if(tag) {
-        console.log(tag);
-      } else{
-        this.$router.replace('/404')
+      const tag = tags.filter(t => t.id === id)[0];
+      if (tag) {
+        this.tag = tag;
+      } else {
+        this.$router.replace('/404');
       }
     }
   }
@@ -61,11 +64,13 @@
             height: 24px;
         }
     }
-    .form-wrapper{
+
+    .form-wrapper {
         background: white;
         margin-top: 8px;
     }
-    .button-wrapper{
+
+    .button-wrapper {
         text-align: center;
         padding: 16px;
         margin-top: 44-16px;
