f9aacfc14767809564ac57b1e97ce2f34e32f27d
Luna <1017929760@qq.com>
2020-04-13 18:11:53 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -21,20 +21,16 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/models/tagListModel';
+  import Button from '@/components/Button.vue';
 
-  @Component
-  export default class extends Vue {
+  @Component({components: {Button}})
+  export default class Labels extends Vue {
     tags = window.tagList;
 
     createTag() {
       const name = window.prompt('请输入标签名');
       if (name) {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-          window.alert('标签名不能重复');
-        } else if (message === 'success') {
-          window.alert('添加成功');
-        }
+        window.createTag(name);
       }
     }
   }
@@ -42,7 +38,7 @@
 
 <style lang="scss" scoped>
     .tags {
-        background: rgb(217,224,235);
+        background: rgb(217, 224, 235);
         font-size: 16px;
         padding-left: 16px;
 
