96847aea3d0042072afea66062bdffcb0cbb3b8e
Luna <1017929760@qq.com>
2020-04-16 16:22:31 +0800

    抽离 Tabs.vue，使用在Statistics.vue里面

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,36 +1,39 @@
 <template>
-    <div>
-        <ul class="types">
-            <li :class="{[classPrefix+'-item']:classPrefix,selected: value==='-'}"
-                @click="selectType('-')">支出
-            </li>
-            <li :class="{[classPrefix+'-item']:classPrefix,selected: value==='+'}"
-                @click="selectType('+')">收入
-            </li>
-        </ul>
-    </div>
+    <ul class="tabs">
+        <li v-for="item in dataSource" :key="item.value"
+            :class="liClass(item)" @click="select(item)">{{item.text}}
+        </li>
+    </ul>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type DataSourceItem = { text: string; value: string }
 
   @Component
-  export default class Types extends Vue {
+  export default class Tabs extends Vue {
+    @Prop({required: true, type: Array})
+    dataSource!: DataSourceItem[];
     @Prop(String) readonly value!: string;
     @Prop(String) classPrefix?: string;
 
-    selectType(type: string) {
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is unknown');
-      }
-      this.$emit('update:value',type);
+    liClass(item: DataSourceItem){
+      return {
+        selected: item.value === this.value,
+        [this.classPrefix + '-tabs-item']: this.classPrefix
+      };
+    }
+
+    select(item: DataSourceItem) {
+      this.$emit('update:value', item.value);
     }
   }
 </script>
 
 <style lang="scss" scoped>
-    .types {
+    .tabs {
         background: rgb(138, 165, 205);
         display: flex;
         text-align: center;
